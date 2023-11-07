f07db4c6176f780a06b76d6e7d1a2f43fa11219c
Carlos Masó Merino <139848427+CarlosMaso@users.noreply.github.com>
2023-11-07 16:19:58 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function desplegar(){
+    var menu=document.getElementById("menu-plegable");
+
+    if(menu.style.display=="block"){
+        menu.style.display="none";
+    }else{
+        menu.style.display="block"
+    }
+    
+}
